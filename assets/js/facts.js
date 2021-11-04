f22f896fbbc5f4747799d54435c1bb232d944bc2
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-11-04 20:17:18 +0530

    update on fact date

--- assets/js/facts.js
+++ assets/js/facts.js
@@ -6,11 +6,15 @@ var autofillDate;
 var factURL = "https://sheets.googleapis.com/v4/spreadsheets/1wV7x9oDaBEOt5oONE82T_hgxyJyXLEoS7fK98zYfWow/values/responses?key="
 var apiKey = "AIzaSyBpaORBU9bCcAMmAbawXlxTpXr1LuAGjWw";
 
-fetch(factURL + apiKey).then((response) => response.json()).then((values) => {
 
-  factData = values;
-  finalDate = moment(factData.values[factData.values.length-1][1], "DD-MM-YYYY")
-  factDate.push(moment(finalDate).add(1, 'days'));
-  autofillDate = factDate[0].format("DD-MM-YYYY");
-  document.getElementById('factDate').value = autofillDate;
-});
+function autoFinalDate() {
+
+  fetch(factURL + apiKey).then((response) => response.json()).then((values) => {
+
+    factData = values;
+    finalDate = moment(factData.values[factData.values.length-1][1], "DD-MM-YYYY")
+    factDate.push(moment(finalDate).add(1, 'days'));
+    autofillDate = factDate[0].format("DD-MM-YYYY");
+    document.getElementById('factDate').value = autofillDate;
+  });
+}
